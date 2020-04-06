98cb8cd20a485999d6ceb2d10eb314f3c11dede9
Lievin Maxime <maxime.lievin@etud.u-picardie.fr>
2020-04-06 18:20:26 +0200

    Click sur les images !

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'starter';
+  title = 'Animals';
+
+  animals = [{
+    nom: "chien",
+    img: "https://static.actu.fr/uploads/2019/04/chein-puppy-jonathan-kriz-flickr-854x678.jpg",
+    desc: "Le chien est un canidé"
+  },{
+    nom: "chat",
+    img: "https://assets.afcdn.com/story/20150511/661351_w980h638c1cx1858cy867.jpg",
+    desc: "Le chat est un félin"
+  },{
+    nom: "cheval",
+    img: "https://i.skyrock.net/0989/74540989/pics/2920279085_1_3.jpg",
+    desc: "Le cheval est un équidé"
+  }];
+
+
+  animalSelected = this.animals[0];
+  affiche(animal){
+    this.animalSelected =  animal;
+  }
+
 }
